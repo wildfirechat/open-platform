f47569ebcad802f5788cb35b24ed335af807ac42
x86 <1960867973@qq.com>
2022-05-26 16:48:13 +0800

    创建应用时，进行参数校验

--- open-web/src/components/page/app/AppDev.vue
+++ open-web/src/components/page/app/AppDev.vue
@@ -18,8 +18,8 @@
                 </el-row>
             </el-card>
             <el-dialog title="创建应用" :visible.sync="createAppDialogVisible">
-                <el-form :model="createAppInfo">
-                    <el-form-item label="应用图标地址" :label-width="formLabelWidth">
+                <el-form :model="createAppInfo" :rules="rules" ref="createAppForm">
+                    <el-form-item label="应用图标地址" :label-width="formLabelWidth" prop="portraitUrl">
                         <el-input v-model="createAppInfo.portraitUrl" autocomplete="off" disabled placeholder="应用图标地址"></el-input>
                         <el-upload
                             class="upload-demo"
@@ -32,26 +32,26 @@
                             <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
                         </el-upload>
                     </el-form-item>
-                    <el-form-item label="应用名称" :label-width="formLabelWidth">
+                    <el-form-item label="应用名称" :label-width="formLabelWidth" prop="name">
                         <el-input v-model="createAppInfo.name" autocomplete="off" placeholder="测试应用"></el-input>
                     </el-form-item>
-                    <el-form-item label="应用描述" :label-width="formLabelWidth">
+                    <el-form-item label="应用描述" :label-width="formLabelWidth" prop="desc">
                         <el-input v-model="createAppInfo.description" autocomplete="off" placeholder="应用的一句话描述"></el-input>
                     </el-form-item>
-                    <el-form-item label="移动端地址" :label-width="formLabelWidth">
+                    <el-form-item label="移动端地址" :label-width="formLabelWidth" prop="mobileUrl">
                         <el-input v-model="createAppInfo.mobileUrl" autocomplete="off" placeholder="https://wildfirechat.cn"></el-input>
                     </el-form-item>
-                    <el-form-item label="桌面端地址" :label-width="formLabelWidth">
+                    <el-form-item label="桌面端地址" :label-width="formLabelWidth" prop="desktopUrl">
                         <el-input v-model="createAppInfo.desktopUrl" autocomplete="off" placeholder="https://wildfirechat.cn"></el-input>
                     </el-form-item>
-                    <el-form-item label="回调/服务端地址" :label-width="formLabelWidth">
+                    <el-form-item label="回调/服务端地址" :label-width="formLabelWidth" prop="serverUrl">
                         <el-input v-model="createAppInfo.serverUrl" autocomplete="off" placeholder="https://wildfirechat.cn"></el-input>
                     </el-form-item>
                     <el-checkbox label="是否是全局应用" v-model="createAppInfo.global"></el-checkbox>
                 </el-form>
                 <div slot="footer" class="dialog-footer">
                     <el-button @click="createAppDialogVisible = false">取 消</el-button>
-                    <el-button type="primary" @click="createApp">确 定</el-button>
+                    <el-button type="primary" @click="submitForm('createAppForm')">确 定</el-button>
                 </div>
             </el-dialog>
 
@@ -109,14 +109,45 @@ export default {
             modifyAppDialogVisible: false,
             createAppInfo: new AppInfo(),
             modifyAppInfo: new AppInfo(),
-            formLabelWidth: '120px'
+            formLabelWidth: '140px',
+
+            rules: {
+                portraitUrl: [
+                    {required: true, message: '请上传头像', trigger: 'blur'},
+                ],
+                name: [
+                    {required: true, message: '请输入应用名称', trigger: 'blur'},
+                    {min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur'}
+                ],
+                desc: [
+                    {required: true, message: '请输入应用描述', trigger: 'blur'},
+                    {min: 1, max: 20, message: '长度在 1 到 10 个字符', trigger: 'blur'}
+                ],
+                mobileUrl: [
+                    {required: true, message: '请输入移动端入口地址', trigger: 'blur'},
+                ],
+                desktopUrl: [
+                    {required: true, message: '请输入桌面端入口地址', trigger: 'blur'},
+                ],
+                serverUrl: [
+                    {required: true, message: '请输入回调地址', trigger: 'blur'},
+                ],
+            }
         }
     },
     methods: {
-        createApp() {
-            this.createAppDialogVisible = false;
-            this.$store.dispatch('createApp', this.createAppInfo);
-            this.createAppInfo = new AppInfo();
+        submitForm(formName) {
+            console.log('submitForm', formName)
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    this.createAppDialogVisible = false;
+                    this.$store.dispatch('createApp', this.createAppInfo);
+                    this.createAppInfo = new AppInfo();
+                } else {
+                    console.log('error submit!!');
+                    return false;
+                }
+            });
         },
         showAppInfo(app) {
             this.modifyAppInfo = app;
@@ -141,10 +172,11 @@ export default {
         },
         beforePortraitUpload(file) {
             const isJPG = file.type === 'image/jpeg';
+            const isPNG = file.type === 'image/png';
             const isLt2M = file.size / 1024 / 1024 < 2;
 
-            if (!isJPG) {
-                this.$message.error('上传头像图片只能是 JPG 格式!');
+            if (!isJPG && !isPNG) {
+                this.$message.error('上传头像图片只能是 JPG/PNG 格式!');
             }
             if (!isLt2M) {
                 this.$message.error('上传头像图片大小不能超过 2MB!');
